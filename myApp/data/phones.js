4ab61c55588a0b747c790b468df0b6827263707f
Pilar Sauthier <pilarsauthier@Pilars-MacBook-Pro.local>
2022-04-07 11:51:34 -0300

    carpeta data y modulo

---
+++ myApp/data/phones.js
@@ -0,0 +1,43 @@
+let phones = {
+    lista : [
+    {
+        marca : "apple",
+        modelo: "iPhone 11",
+        anio: 2020,
+        color: "negro",
+    },
+    {   
+         marca : "apple",
+        modelo: "iPhone 8",
+        anio: 2018,
+        color: "negro"
+    },
+    {
+        marca : "apple",
+        modelo: "iPhone 12",
+        anio: 2021,
+        color: "gris"
+    },
+    {
+        marca : "apple",
+        modelo: "iPhone X",
+        anio: 2018,
+        color: "blanco"
+    },
+    {
+        marca : "apple",
+        modelo: "iPhone 13",
+        anio: 2022,
+        color: "blanco"
+    },
+    {
+        marca : "apple",
+        modelo: "iPhone 12",
+        anio: 2021,
+        color: "gris"
+    }
+
+    ]
+}
+
+module.exports = phones;
