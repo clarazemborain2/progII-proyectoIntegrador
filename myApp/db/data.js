1732dff211664a49895eb0c87d5d70c2a4bddc2d
Pilar Sauthier <pilarsauthier@Pilars-MacBook-Pro.local>
2022-04-08 11:58:50 -0300

    data.ejs

--- myApp/db/data.js
+++ myApp/db/data.js
@@ -19,36 +19,70 @@ usuario: {
         modelo: "iPhone 11",
         anio: 2020,
         color: "negro",
+        imagen: ''
     },
     {   
          marca : "apple",
         modelo: "iPhone 8",
         anio: 2018,
-        color: "negro"
+        color: "negro",
+        imagen: ''
     },
     {
         marca : "apple",
         modelo: "iPhone 12",
         anio: 2021,
-        color: "gris"
+        color: "gris",
+        imagen: ''
     },
     {
         marca : "apple",
         modelo: "iPhone X",
         anio: 2018,
-        color: "blanco"
+        color: "blanco",
+        imagen: ''
     },
     {
         marca : "apple",
         modelo: "iPhone 13",
         anio: 2022,
-        color: "blanco"
+        color: "blanco",
+        imagen: ''
     },
     {
         marca : "apple",
         modelo: "iPhone 12",
         anio: 2021,
-        color: "gris"
+        color: "gris",
+        imagen: ''
+    },
+    {
+        marca : "apple",
+        modelo: "iPhone 5",
+        anio: 2014,
+        color: "negro",
+        imagen: ''
+    },
+    {
+        marca : "apple",
+        modelo: "iPhone X",
+        anio: 2018,
+        color: "negro",
+        imagen: ''
+    },
+    {
+        marca : "apple",
+        modelo: "iPhone 8",
+        anio: 2018,
+        color: "blanco",
+        imagen: ''
+    },
+    {
+        marca : "apple",
+        modelo: "iPhone 11",
+        anio: 2019,
+        color: "blanco",
+        imagen: ''
     }
 
     ]
