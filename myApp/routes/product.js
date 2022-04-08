88fba7553670ccd66ee5ac20e9a911e80ef36f92
Pilar Sauthier <pilarsauthier@Pilars-MacBook-Pro.local>
2022-04-08 19:18:01 -0300

    update

--- myApp/routes/product.js
+++ myApp/routes/product.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 let productoController = require('../controllers/productosController');
 
-router.get('/producto', productoController.producto);
+router.get('/', productoController.producto);
 
 router.get('/agregarProducto', productoController.productAdd);
 
