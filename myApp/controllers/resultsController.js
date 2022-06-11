2b4b2140f9a0595e8255834043efe92c334c855e
Franco Galluzzo <fgalluzzo@udesa.edu.ar>
2022-06-11 20:06:30 -0300

    errores y busqueda

--- myApp/controllers/resultsController.js
+++ myApp/controllers/resultsController.js
@@ -1,8 +1,20 @@
 let resultsController = {
     results : function(req, res) {
          res.render('search-results')
+ },
+ showOne: (req, res) => {
+     let busqueda = req.query.producto;
+     productos.findOne({
+         where: [
+             {nombre : {
+                 [op.like] : "%" + busqueda + ""
+             }} /*op.like no funciona*/
+         ]
+     }).then((result) => {
+         return res.send(result);
+     })
  }
 
-}
+};
  
  module.exports = resultsController
