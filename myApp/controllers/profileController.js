982f7d0d0db3348240e4539c4c752aa38cc36e6b
pilarsauthier <pilarsauthier@gmail.com>
2022-06-17 18:33:08 -0300

    register

--- myApp/controllers/profileController.js
+++ myApp/controllers/profileController.js
@@ -1,4 +1,4 @@
-const dataModule = require('../db/data')// no sirve mas
+const db = require('../database/models');
 
 let perfilController = {
 
@@ -23,8 +23,20 @@ let perfilController = {
         res.render('register');
     },
     procesarRegister : function(req, res) {
-        console.log(req.body)
-        res.redirect('/')
+        let usuarioNuevo = {
+            email: req.body.email,
+            usuario: req.body.nombre,
+            contrasenia: req.body.contrasenia,
+            fecha_de_nacimiento: req.body.fecha_de_nacimiento,
+            nro_de_documento: req.body.nro_de_documento,
+            foto_de_perfil: req.body.foto_de_perfil,
+        }
+        db.Usuario.create(usuarioNuevo)
+        .then(function (results) {
+            return res.redirect('/profile')
+        }
+        )
+       
     }
  }
  
